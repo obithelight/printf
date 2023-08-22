c0996b0ae64cfc6baba31848e352901bce61c7be
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-22 06:58:46 -0700

    _printf.c function file

---
+++ _printf.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * _printf - writes formatted text to the stdout
+ * @format: character, string, or formatted text
+ *
+ * Return: number of bytes of format
+ */
+
+int _printf(const char *format, ...)
+{
+	int obi = 0;
+	char b;
+	char *uncleTed;
+
+	va_list hufano;
+
+	if (format == NULL)
+	{
+		exit(98);
+	}
+
+	va_start(hufano, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			obi++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+			{
+				break;
+			}
+
+			if (*format == 'c')
+			{
+				b = va_arg(hufano, int);
+				write(1, &b, 1);
+			}
+
+			else if (*format == 's')
+			{
+				uncleTed = va_arg(hufano, char *);
+				write(1, uncleTed, lengthOfStr(uncleTed));
+				obi += lengthOfStr(uncleTed);
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				obi++;
+			}
+		}
+		format++;
+	}
+	va_end(hufano);
+
+	return (obi);
+}
