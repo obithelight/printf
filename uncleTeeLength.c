8f6153cc76fdb3de25f7693c2721b7fd9912cf71
“UncleTed01” <godswilokoyo@gmail.com>
2023-08-22 04:47:09 -0700

    uncleTeelength.c

---
+++ uncleTeeLength.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * lengthOfStr - returns the length of a string
+ * @obi: string
+ *
+ * Return: 0
+ */
+
+int lengthOfStr(char *obi)
+{
+        int start;
+        int end = 0;
+
+        for (start = 0; obi[start] != '\0'; start++)
+        {
+                end++;
+        }
+        return (end);
+}
