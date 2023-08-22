f06cf409600b6d82a61c8834d0121f64208fbe64
“UncleTed01” <godswilokoyo@gmail.com>
2023-08-22 04:45:19 -0700

    main.hfile

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int lengthOfStr(char *obi);
+
+#endif /* MAIN_H */
