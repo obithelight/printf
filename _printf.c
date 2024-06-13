091eb7433300c2bf4984814aaf5ffe7ba15d3214
Obi Chukwu <kennetho.onu@gmail.com>
2024-06-13 04:04:36 +0100

    updated _printf function to handle flags correctly

--- _printf.c
+++ _printf.c
@@ -1,49 +1,49 @@
 #include "main.h"
+
 /**
  * _printf - prints formatted to the stdout just like the printf
  * @format: formatted text to be checked before printing
+ *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
+	va_list list;
+	int flag = 0, len = 0, i = 0, j, arr_len;
 	print_ft func[] = {
 		{'c', print_char},
 		{'s', print_str},
 		{'%', print_modulo},
+		/* {'d', print_int}, */
 	};
-	va_list list;
-	int len = 0, i = 0, j, arr_len = sizeof(func) / sizeof(func[0]);
+
+	/* check if format == NULL or if (format == '%' && '\0') */
+	if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
+		return (-1);
 
 	va_start(list, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 	while (format && format[i])
 	{
-		if (format[i] != '%')
+		if (format[i] != '%') /* if format is normal strings all through */
+			putchar(format[i]), len++;
+		else /* if a '%' sign is found, increment and check the next character*/
 		{
-			putchar(format[i]);
-			len++;
-		}
-		else
-		{
-			i++;
-			for (j = 0; j < arr_len; j++)
+			j = 0, i++;
+			arr_len = sizeof(func) / sizeof(func[0]);
+			while (j < arr_len)
 			{
 				if (format[i] == func[j].symbol)
 				{
 					len += func[j].print(list);
+					flag = 1; /* indicates a match was found*/
 					break;
-				}
+				} j++;
 			}
-			if (format[i] != func[j].symbol)
-			{
-				i--;
-				putchar(format[i]);
-				len++;
-			}
-		}
-		i++;
+			if (flag == 0) /* if a match was not found, decrement and print format[i] */
+				putchar(format[--i]), len++;
+		} i++;
 	}
+
 	va_end(list);
 	return (len);
 }
